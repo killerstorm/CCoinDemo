eeaa9659b2752adf69cffd8c14d448937f0ed277
Alex Mizrahi <alex.mizrahi@gmail.com>
2013-10-09 16:30:01 +0300

    wallet model

--- wallet_model.py
+++ wallet_model.py
@@ -1,24 +1,40 @@
-class ColorSet(object):
-    pass
+# Wallet Model: part of Wallet MVC structure
 
+# model provides facilities for working with addresses, coins and asset definitions,
+# but it doesn't implement high-level operations (those are implemented in controller)
+
+class ColorSet(object):
+    """a set of colors which belong to certain asset, 
+    it can be used to filter addresses and UTXOs"""
+    def __init__(self, model, color_desc_list):
+        self.color_id_set = set()
+        colormap = model.get_color_map()
+        for color_desc in color_desc_list:
+            self.color_id_set.add(colormap.resolve_color_desc(color_desc))
+
+    def has_color_id(self, color_id):
+        return (color_id in self.color_id_set)
+    def intersects(self, other):
+        return len(self.color_id_set & other.color_id_set) > 0
 
 class AssetDefinition(object):
-    def __init__(self, config):
-        self.moniker = config.get('moniker')
+    def __init__(self, model, params):
+        self.moniker = params.get('moniker')
+        self.color_set = ColorSet(model, params.get('color_set'))
     def get_moniker(self):
         return self.moniker
     def get_color_set(self):
-        return ColorSet()
+        return self.color_set
     def get_utxo_value(self):
         pass
 
 class AssetDefinitionManager(object):
     """manages asset definitions"""
-    def __init__(self, config):
+    def __init__(self, model, config):
         self.asset_definitions = []
         self.assdef_by_moniker = {}
-        for ad_config in config.get('asset_definitions'):
-            assdef = AssetDefinition(ad_config)
+        for ad_params in config.get('asset_definitions'):
+            assdef = AssetDefinition(model, ad_params)
             asset_definitions.append(assdef)
             moniker = assdef.get_moniker()
             if moniker:
@@ -27,6 +43,46 @@ class AssetDefinitionManager(object):
                 self.assdef_by_moniker[moniker] = assdef
     def get_asset_by_moniker(self, moniker):
         return self.assdef_by_moniker.get(moniker)
+
+
+class Address(object):
+    def __init__(self, model, params):
+        self.color_set = ColorSet(params.get('color_set'))
+    def get_color_set(self):
+        return self.color_set
+                                  
+
+class WalletAddressManager(object):
+    def __init__(self, model, config):
+        self.addreses = []
+        for addr_params in config.get('addresses'):
+            address = Address(model, addr_params)
+            self.addresses.append(address)
+        
+    def get_addresses_for_color_set(self, color_set):
+        return [addr for addr in self.addresses 
+                if color_set.intersects(address.get_color_set())]
+
+class ColoredCoinContext(object):
+    def __init__(self, config):
+
+        params = config.get('ccc')
+
+        from coloredcoinlib import agent
+        from coloredcoinlib import blockchain
+        from coloredcoinlib import builder
+        from coloredcoinlib import colordef
+        from coloredcoinlib import store
+        from coloredcoinlib import colormap
+
+        self.blockchain_state = blockchain.BlockchainState(params.get('bitcoind_url'))
+        
+        self.store_conn = store.DataStoreConnection(params.get("color.db", "color.db"))
+        self.cdstore = store.ColorDataStore(self.store_conn.conn)
+        self.metastore = store.ColorMetaStore(self.store_conn.conn)
+
+        self.colormap = colormap.ColorMap(self.metastore)
+
 
 class CoinQuery(object):
     """can be used to request UTXOs satisfying certain criteria"""
@@ -42,38 +98,27 @@ class CoinQuery(object):
             utxos.extend(address.getUTXOs(color_set))
         return utxos
 
-class Address(object):
-    pass
-
-class WalletAddressManager(object):
-    def __init__(self, config):
-        self.addreses = []
-        for addr_conf in config.get('addresses'):
-            address = Address(addr_conf)
-            self.addresses.append(address)
-        
-    def get_addresses_for_color_set(self, color_set):
-        return [addr for addr in self.addresses if color_set.contains(address.colorid)]
-
-class WalletController(object):
-    def __init__(self, model):
+class CoinQueryFactory(object):
+    def __init__(self, model, config):
         self.model = model
-
-    def get_balance(self, asset):
-        cq = CoinQuery(self.model, asset)
-        utxo_list = cq.get_result()
-        value_list = [asset.get_utxo_value(utxo) for utxo in utxo_list]
-        return sum(value_list)
+    def make_query(self, query):
+        return CoinQuery(self.model, query.get("asset"))
 
 class WalletModel(object):
     def __init__(self, config):
-        self.ass_def_man = AssetDefinitionManager(config)
-        self.address_man = WalletAddressManager(config)
+        self.ccc = ColoredCoinContext(config)
+        self.ass_def_man = AssetDefinitionManager(self, config)
+        self.address_man = WalletAddressManager(self, config)
+        self.coin_query_factory = CoinQueryFactory(self, config)
+    def get_coin_query_factory(self):
+        return self.coin_query_factory
+    def make_coin_query(self, params):
+        return self.coin_query_factory.make_query(params)
     def get_asset_definition_manager(self):
         return self.ass_def_man
     def get_address_manager(self):
         return self.address_man
+    def get_color_map(self):
+        return self.ccc.colormap
 
-class WalletInterface(object):
-    pass
 
